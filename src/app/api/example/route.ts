ae532dd755759a58d109160bfe5451d4ab36c960
Malik Elbadri <elbadrimalik@gmail.com>
2023-11-07 17:25:53 +0300

    Added prisma to the project

--- src/app/api/example/route.ts
+++ src/app/api/example/route.ts
@@ -1,3 +1,14 @@
+import { PrismaClient } from "@prisma/client";
+
 export async function GET(request: Request) {
-  return Response.json("Hello");
+  const prisma = new PrismaClient();
+  // await prisma.user.create({
+  //   data: {
+  //     email: "asdf@adsf.com",
+  //     name: "malik",
+  //   },
+  // });
+  const user = await prisma.user.findMany();
+
+  return Response.json(user);
 }
